ac0b5874729d478809a715e7d7c18e81d6b57965
hendrash <hendrash@mail.gvsu.edu>
2021-03-29 12:01:27 -0400

    glow added

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { UniswapApi } from './api/uniswap-api';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { UniswapApi } from "./api/uniswap-api";
+import { Box, Button } from "@material-ui/core";
+import {MdShowChart} from "react-icons/md";
 function App() {
   return (
     <div className="App-header App">
+      <p className="glow">MY DEFI NOTES AND ANALYSIS</p>
       <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My decentralized notes
-        </p>
-      </header> 
-      Arbitrage
-
-      <UniswapApi/>
-  
+      </header>
+      <Box className="boxStyle">
+        <span >Swap Charts </span>
+      <MdShowChart/>
+         </Box>
     </div>
   );
 }
