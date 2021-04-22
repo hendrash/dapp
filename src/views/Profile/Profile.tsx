04a913b6540d41a5aec4a04683e07b33be8a26e7
hendrash <hendrash@mail.gvsu.edu>
2021-04-22 17:29:46 -0400

    profile

--- src/views/Profile/Profile.tsx
+++ src/views/Profile/Profile.tsx
@@ -1,9 +1,4 @@
-import { withAuthenticator } from 'aws-amplify-react'
-import { Theme } from './config'
-import Amplify from 'aws-amplify'
-import awsconfig from '../../aws-exports'
-Amplify.configure(awsconfig)
 const Profile: React.FC=()=>{ 
 return (<></>)
 }
-export default withAuthenticator(Profile)
+export default Profile
